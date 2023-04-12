b0107c3b46c94e97ad4df67c56ae44e72468571b
Konstantin <talalaev22@.gmail.com>
2023-04-12 13:31:08 +0500

    ДЗ готово!!!

---
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatedService.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.calculator.service;
+
+public interface CalculatedService {
+    int plus(int x, int y);
+    int minus(int x, int y);
+    int multiply(int x, int y);
+    int divide(int x, int y);
+}
